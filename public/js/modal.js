e12323ab53a696cfb29d3fc5ed1f1cd0e6b7e6ac
Asim Zaidi <asimzaidih@gmail.com>
2019-01-08 17:43:04 -0800

    text fixes

--- public/js/modal.js
+++ public/js/modal.js
@@ -1,10 +1,10 @@
-console.log("innissss");
-
 // Get the modal
 var modal = document.getElementById('myModal');
 
 // Get the button that opens the modal
 var btn = document.getElementById("myBtn");
+var btn2 = document.getElementById("myBtn2");
+
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
@@ -12,6 +12,10 @@ var span = document.getElementsByClassName("close")[0];
 // When the user clicks the button, open the modal 
 btn.onclick = function() {
   modal.style.display = "block";
+}
+
+btn2.onclick = function() {
+    modal.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
